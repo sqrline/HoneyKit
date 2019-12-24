0070469908d20d39e345cf19861daf1b5c2ff314
AlekseyPleshkov <im@alekseypleshkov.ru>
2019-12-24 21:24:52 +0300

    Initial Commit

---
+++ HoneyKit/HoneyKit.h
@@ -0,0 +1,19 @@
+//
+//  HoneyKit.h
+//  HoneyKit
+//
+//  Created by Aleksey Pleshkov on 24.12.2019.
+//  Copyright © 2019 SQR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HoneyKit.
+FOUNDATION_EXPORT double HoneyKitVersionNumber;
+
+//! Project version string for HoneyKit.
+FOUNDATION_EXPORT const unsigned char HoneyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HoneyKit/PublicHeader.h>
+
+
