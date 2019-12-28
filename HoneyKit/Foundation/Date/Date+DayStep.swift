538001af44a9b1ec6de02c369f414c0101741ec7
AlekseyPleshkov <im@alekseypleshkov.ru>
2019-12-28 14:14:47 +0300

    [Foundation] Add Date+DayStep

--- HoneyKit/Foundation/Date/Date+DayStep.swift
+++ HoneyKit/Foundation/Date/Date+DayStep.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+extension Date {
+  
+  // MARK: - Enums
+  
+  /// Direction for adding days
+  enum StepDirection {
+    case forward
+    case backward
+  }
+  
+  // MARK: - Private Properties
+  
+  private var calendar: Calendar {
+    return Calendar.current
+  }
+  
+  // MARK: - Public Methods
+  
+  /// Returns date after adding step to exists date based on direction
+  ///
+  /// # Example
+  /// ```
+  /// let currentDate = Date()
+  /// let tomorrowDate = currentDate.step(.forward)
+  /// let afterTomorrowDate = currentDate.step(.forward, count: 2)
+  /// let yesterdayDate = currentDate.step(.backward)
+  /// ```
+  /// - Parameters:
+  ///   - direction: Step direction
+  ///   - count: Numbers of days for take a step
+  /// - Returns: Returns date after step
+  func step(_ direction: StepDirection, count: Int = 1) -> Date {
+    let stepValue = direction == .forward ? count : -count
+    
+    return calendar.date(byAdding: .day, value: stepValue, to: self) ?? self
+  }
+}
