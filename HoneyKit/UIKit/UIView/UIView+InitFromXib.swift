247b1bf375f511729a9359f32df79894cacc595a
Vladimir Pchelyakov <v.pchelyakov@yandex.ru>
2020-02-09 23:42:03 +0300

    [UIKit] Add InitFromXib

---
+++ HoneyKit/UIKit/UIView/UIView+InitFromXib.swift
@@ -0,0 +1,36 @@
+//
+//  UIView+InitFromXib.swift
+//  HoneyKit
+//
+//  Created by Vladimir Pchelyakov on 09.02.2020.
+//  Copyright © 2020 SQR. All rights reserved.
+//
+
+import UIKit
+
+/// Instantiates view from xib.
+public extension UIView {
+  
+  /// Instantiates view from xib.
+  ///
+  /// - Important: Class name, xib file name, root view's (in interface builder) custom class type must have same name.
+  /// Example: class CustomView, CustomView.xib, interface builder -> root view's custom class type - CustomView
+  ///
+  /// # Example
+  /// ```
+  /// let customView = CustomView.instantiate()
+  /// // Instance of CustomView
+  /// ```
+  /// - Returns: View from xib.
+  static func instantiate() -> Self {
+    let name = String(describing: self)
+    let nib = UINib(nibName: name, bundle: nil)
+    
+    guard let view = nib.instantiate(withOwner: nil, options: nil).first as? Self else {
+      fatalError("Unexpected view \(name)")
+    }
+    
+    return view
+  }
+  
+}
